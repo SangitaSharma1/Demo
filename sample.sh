3166406b415ff677de5c79a3953807a692c91fcb
SangitaSharma1 <40599359+SangitaSharma1@users.noreply.github.com>
2019-04-02 07:37:42 +0000

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo su -;
 cd /root;
 awk '/dir/' <(ls -lrth)| awk '{print $9}' > /tmp/reponames.txt;
 awk -v sz="$(wc -l < /tmp/reponames.txt)" 'NR<=(sz-4)' /tmp/reponames.txt  > /tmp/finalized.txt;
